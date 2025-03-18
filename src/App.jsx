0c57cbddb61df571d2bc9e3648d9c7c61bd71722
programadormovel <programadormovel@gmail.com>
2025-03-18 08:29:13 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,28 @@ function App(){
 
   const [nome, setNome] = useState('Ronaldo');
 
+  const Formulario = () => {
+    return (
+      <>
+        <h4>Formulário de Inscrição</h4>
+        <input className="Nome"
+            name = "nome"
+            placeholder="Digite seu nome..."
+            onChange={(e)=>setNome(e.target.value)}
+            type="text" />
+        <button onClick={()=>{alert(nome)}}>
+          CLIQUE AQUI
+        </button>
+      </>
+    )
+  }
+
   return (
     <div>
       <h3>Pizzaria 2A</h3>
-      <input className="Nome"
-          name = "nome"
-          placeholder="Digite seu nome..."
-          onChange={(e)=>setNome(e.target.value)}
-          type="text" />
-        <button onClick={()=>{alert(nome)}}>
-        CLIQUE AQUI
-      </button>
+      <Formulario />
+      <Formulario />
+      <Formulario />
     </div>
   )
 }
