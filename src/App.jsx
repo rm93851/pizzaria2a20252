2cce17f48e84c24a263fac9d23026d9119127e4f
programadormovel <programadormovel@gmail.com>
2025-04-02 08:14:50 -0300

    passagem de props

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ function App(){
 
   const [nome, setNome] = useState('Ronaldo');
 
-  const Formulario = () => {
+  const Formulario = (props) => {
     return (
       <>
         <h4>Formulário de Inscrição</h4>
         <input className="Nome"
             name = "nome"
             placeholder="Digite seu nome..."
-            onChange={(e)=>setNome(e.target.value)}
+            onChange={(e)=>props.nome = e.target.value}
             type="text" />
-        <button onClick={()=>{alert(nome)}}>
+        <button onClick={()=>{
+          (props.texto != null) ? alert(props.texto) : alert(props.nome)
+          }}>
           CLIQUE AQUI
         </button>
       </>
@@ -23,9 +25,10 @@ function App(){
   return (
     <div>
       <h3>Pizzaria 2A</h3>
-      <Formulario />
-      <Formulario />
-      <Formulario />
+      <Formulario texto="Texto enviado pelo App" />
+      <Formulario texto="Santos meu amor" />
+      <Formulario texto="Novo texto enviado pelo App" />
+      <Formulario nome={nome} />
     </div>
   )
 }
