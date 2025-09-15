a23ab46dd1552e1f5adef2a93f25ba9cd5974982
RihannaMattos <hannawzzy@gmail.com>
2025-09-15 12:28:36 -0300

    consumo

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,4 +1,27 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
+
 const Produtos = () => {
+
+    const [dados, setDados] = useState([]);
+
+    const pegarPizzas = async () => {
+
+        await axios.get("http://172.19.0.49/pizzariaoficial/api/v1/produto", 
+            {withCredentials: true}
+        )
+        .then((response) => setDados(JSON.stringify(response.data)))
+        .catch((error) => console.log(error))
+    }
+
+    useEffect(()=>{
+        pegarPizzas();
+    }, [])
+
+    useEffect(()=>{
+        console.log(dados)
+    }, [dados])
 
     // Objeto de produtos da lista
     const pizzas = [
