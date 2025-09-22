f0f53c3aaec0029ce027295cb2bdb6c406827025
rm93851 <rm93851@estudante.fieb.edu.br>
2025-09-22 12:22:55 -0300

    cadastro

---
+++ src/pages/produtos/cadastro.jsx
@@ -0,0 +1,22 @@
+import './produtos.css'
+const CadastroProduto = () => {
+
+    return(
+        <div className="CadastroProduto">
+
+            <h3>Cadastro de produtos</h3>
+            <input type="text" id="id"placeholder="0" disabled/>
+            <input type="text" id="nome"placeholder="nome do produto"/>
+            <input type="text" id="descricao"placeholder="descricao"/>
+            <input type="text" id="tipo"placeholder="tipo do produto"/>
+            <input type="text" id="preco"placeholder="valor do produto"/>∟
+            <input type="text" id="categoriaid"placeholder="1" disabled/>
+
+            <input type="button" value="Cadastrar" 
+            onClick = {()=>{alert('Cadastrar Produto')}} />
+            
+        </div>
+    )
+}
+
+export default CadastroProduto
--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,11 +1,19 @@
+// Hooks do react - para controlar renderizacao e estados
 import {useState, useEffect} from 'react'
+//Ferramenta para consumir as rotas do backend
 import axios from 'axios'
  
 const Produtos = () => {
- 
-    const [dados, setDados] = useState([]);
- 
+  //Um estado para controlar os dados
+    const [dados, setDados] = useState(['Mucarela','Calabresa']);
+
+ // Controla a renderizacao da pagina
     useEffect(()=>{  
+        // GET= busca os dados da rota(endpoint)
+        // devolve dados(data) e codigo da resposta(status)
+        // then e uma promise(promessa) requisicao assincrona
+        // catch captura o erro da requisicao
+        
         axios.get("http://172.19.0.49/pizzariaoficial/api/v1/produto")
         .then((response) => {
             console.log(response.data.data)
